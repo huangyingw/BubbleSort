d0d305695667cb8769830b5e62277b2833992d5c
huangyingw@linux_programming <huangyingw@gmail.com>
2014-01-15 23:26:15 -0800

    n

--- src/bubbleSortTest.java
+++ src/bubbleSortTest.java
@@ -1,11 +1,22 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 
 public class bubbleSortTest {
 
-  @Test
-  public void test() {
-    assertTrue(true);
-  }
+	@Test
+	public void test() {
+
+		int[] expect = new int[] { 1, 2, 3, 4, 5 };
+
+		int[] data = new int[] { 5, 4, 3, 2, 1 };
+
+		BubbleSort b = new BubbleSort(data);
+		b.sort();
+		System.out.println(expect.toString());
+		System.out.println(b.getData().toString());
+		assertTrue(Arrays.equals(b.getData(), expect));
+	}
 }
